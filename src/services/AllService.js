29e9baa1e603ad8eaef87ae8842abb16fc7ec98d
hamidrezajalivar <hamidrezajalivar17830@gmail.com>
2022-10-30 15:27:40 +0330

    set header and axios interceptor

--- src/services/AllService.js
+++ src/services/AllService.js
@@ -7,12 +7,20 @@ export function getSingleData(id) {
     return http.get(`/${id}`)
 }
 
-export function postData(user) {
-    return http.post(``,{...user})
-}
 export function putData(id,user) {
     return http.put(`/${id}`,{...user})
 }
 export function deleteData(id) {
     return http.delete(`/${id}`)
+}
+
+
+export function postData(user) {
+    const token="SECURE TOKEN !";
+    const header={
+        headers:{
+            Authorization: `Bearer ${token}`,
+        },
+    }
+    return http.post(``,{...user},header)
 }
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,6 +2,27 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://63581241c27556d289368088.mockapi.io/api/v1/users';
 
+axios.interceptors.request.use(
+    (request)=>{
+        console.log(request);
+        return request;
+    },
+    (error)=>{
+        console.log(error);
+        return Promise.reject();
+    }
+)
+
+axios.interceptors.response.use(
+    (response)=>{
+        console.log(response);
+        return response;
+    },
+    (error)=>{
+        console.log(error);
+        return Promise.reject();
+    }
+)
 
 const http={
     get:axios.get,
